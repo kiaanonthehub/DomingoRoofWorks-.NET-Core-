0344a0752c0e1aad775008689de2407e3d901e5a
kiaanonthehub <kiaanmaharaj7@gmail.com>
2021-07-07 13:11:20 +0200

    published

--- Models/Domingo_Roof_WorksContext.cs
+++ Models/Domingo_Roof_WorksContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using DomingoRoofWorks.Models;
 
 #nullable disable
 
@@ -200,5 +201,7 @@ namespace DomingoRoofWorks.Models
         }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public DbSet<DomingoRoofWorks.Models.Login> Login { get; set; }
     }
 }
